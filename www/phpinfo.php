fac635d2c9640a1f520a06dd06ca307dd8e579d4
b2blosangeles <b2blosangeles@gmail.com>
2020-04-03 11:50:51 -0700

    Update phpinfo.php

--- www/phpinfo.php
+++ www/phpinfo.php
@@ -6,7 +6,7 @@ $password = $_GET["password"];
 $dbname = "mysql";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
