cfa2ff4373b97dfb49dc0147d4bbb87d93d9c94c
b2blosangeles <b2blosangeles@gmail.com>
2020-04-03 16:48:00 -0700

    Update phpinfo.php

--- www/phpinfo.php
+++ www/phpinfo.php
@@ -8,14 +8,13 @@ $dbname = "mysql";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-echo "test 2";
-
 // Check connection
-/*
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
+echo "test 3";
+/*
 $sql = "SELECT id, firstname, lastname FROM MyGuests";
 $result = $conn->query($sql);
 
