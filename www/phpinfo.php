ac21f1488fbc66fb83fa12fbe1319ea364e45546
b2blosangeles <b2blosangeles@gmail.com>
2020-04-03 11:47:08 -0700

    Update phpinfo.php

--- www/phpinfo.php
+++ www/phpinfo.php
@@ -1,5 +1,28 @@
+
 <?php
+$servername = "142.93.73.66";
+$username = "root";
+$password = $_GET["password"];
+$dbname = "mysql";
 
-phpinfo();
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
+$sql = "SHOW TABLES";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+       print_r($row);
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
+
