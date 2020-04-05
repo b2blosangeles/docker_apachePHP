1912f1686325730fc72866c1a31b5c45887678f6
b2blosangeles <b2blosangeles@gmail.com>
2020-04-04 17:15:40 -0700

    Update phpinfo.php

--- www/phpinfo.php
+++ www/phpinfo.php
@@ -8,8 +8,8 @@ $password = 'Trinet2020#';
 $dbname = "test";
 // $_GET["password"]
 echo $_GET["password"];
-echo "test 313";
-/*
+echo "test 323";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
@@ -18,7 +18,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "test 3";
-
+/*
 $sql = "SELECT id, firstname, lastname FROM MyGuests";
 $result = $conn->query($sql);
 
@@ -30,8 +30,8 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
-
-$conn->close();
 */
+$conn->close();
+
 ?>
 
