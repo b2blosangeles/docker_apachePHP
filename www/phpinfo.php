09df4607159a05d04dab1b364e05be0d467d75df
b2blosangeles <b2blosangeles@gmail.com>
2020-04-05 10:59:42 -0700

    Update phpinfo.php

--- www/phpinfo.php
+++ www/phpinfo.php
@@ -12,7 +12,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
+echo "errr";
 
 $sql = "SELECT * FROM `time_zone`;";
 $result = $conn->query($sql);
